cfc230a28696a6f3f019f4d330f2b419fc8bd982
Euler Reis das Virgens Filho <euler.filho@softplan.com.br>
2020-09-22 21:34:54 -0300

    add prop onSearch

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Input, Button } from '@material-ui/core';
@@ -8,21 +8,34 @@ import { useTranslation } from 'react-i18next';
 
 import styles from './styles';
 
-const InputContainer = ({ classes, search }) => {
+const InputContainer = ({ classes, search, onSearch }) => {
+  const [value, setValue] = useState('');
   const { t } = useTranslation();
 
+  const onSubmit = e => {
+    e.preventDefault();
+    onSearch(value);
+  };
+
+  const onChange = e => {
+    setValue(e.target.value);
+  };
+
   return (
-    <div className={classes.root}>
+    <form className={classes.root} onSubmit={onSubmit}>
       <Input
+        id='search'
         className={classes.search}
         placeholder={t('SEARCH')}
         disableUnderline={true}
+        onChange={onChange}
       />
       {search &&
-        <Button className={classes.button}>
+        <Button className={classes.button} type='submit'>
           <Search style={{ color: 'white' }} />
-        </Button>}
-    </div>
+        </Button>
+      }
+    </form>
   );
 };
 
